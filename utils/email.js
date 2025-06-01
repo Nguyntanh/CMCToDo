e49b417220609d808d4cd0e959ba572c96af98c5
TunAng <nguyntanh2k5@gmail.com>
2025-06-01 18:23:42 +0700

    b

--- utils/email.js
+++ utils/email.js
@@ -3,13 +3,18 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.GMAIL_USER, // Email của bạn
-    pass: process.env.GMAIL_PASS // Mật khẩu ứng dụng của Gmail
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS
   }
 });
 
+// Thêm log để kiểm tra
+console.log('GMAIL_USER:', process.env.GMAIL_USER);
+console.log('GMAIL_PASS:', process.env.GMAIL_PASS);
+
 const sendEmail = async (to, subject, text) => {
   try {
+    console.log(`Sending email to ${to} with subject: ${subject}`);
     await transporter.sendMail({
       from: `"CMC To Do" <${process.env.GMAIL_USER}>`,
       to,
